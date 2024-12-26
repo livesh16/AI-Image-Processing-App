7c62b7916bf3bec7b29494957dc715acf2bb648c
Livesh Armoogum <livesh.armoogum@outlook.com>
2024-12-26 12:46:24 -0500

    Protect all page contents in route layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { SignedIn } from '@clerk/nextjs'
 import React from 'react'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
@@ -7,11 +8,13 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     <main className='root'>
       <Sidebar /> {/* Sidebar will be displayed for large screen only, while MobileNav will be hidden for large screens only. So they won't appear together */}
       <MobileNav />
-      <div className='root-container'>
-          <div className='wrapper'>
-              {children}
-          </div>
-      </div>
+      <SignedIn>
+        <div className='root-container'>
+            <div className='wrapper'>
+                {children}
+            </div>
+        </div>
+      </SignedIn>
     </main>
   )
 }
